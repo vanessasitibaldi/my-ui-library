276eb493e88736d7f52537f78e61e2f8c4b35458
vanessasitibaldi <vanessasitibaldi>
2025-05-24 17:07:54 -0300

    limpa codigo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-// Reexport the native module. On web, it will be resolved to MyUiLibraryModule.web.ts
-// and on native platforms to MyUiLibraryModule.ts
-export { default } from './MyUiLibraryModule';
-export { default as MyUiLibraryView } from './MyUiLibraryView';
-export * from  './MyUiLibrary.types';
+
