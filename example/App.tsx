2b07071b150657dd688e260a46ff5ce7402fb79d
vanessasitibaldi <vanessasitibaldi>
2025-05-24 18:54:10 -0300

    ajusta example

--- example/App.tsx
+++ example/App.tsx
@@ -1,49 +1,14 @@
-import { useEvent } from 'expo';
-import MyUiLibrary, { MyUiLibraryView } from 'my-ui-library';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { Button } from '@vanessa.sitibaldi/mylibraryteste';
 
 export default function App() {
-  const onChangePayload = useEvent(MyUiLibrary, 'onChange');
-
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-        <Group name="Constants">
-          <Text>{MyUiLibrary.PI}</Text>
-        </Group>
-        <Group name="Functions">
-          <Text>{MyUiLibrary.hello()}</Text>
-        </Group>
-        <Group name="Async functions">
-          <Button
-            title="Set value"
-            onPress={async () => {
-              await MyUiLibrary.setValueAsync('Hello from JS!');
-            }}
-          />
-        </Group>
-        <Group name="Events">
-          <Text>{onChangePayload?.value}</Text>
-        </Group>
-        <Group name="Views">
-          <MyUiLibraryView
-            url="https://www.example.com"
-            onLoad={({ nativeEvent: { url } }) => console.log(`Loaded: ${url}`)}
-            style={styles.view}
-          />
-        </Group>
+        <Text style={styles.header}>Vanessa</Text>
+        <Button title="Click me" />
       </ScrollView>
     </SafeAreaView>
-  );
-}
-
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
-    </View>
   );
 }
 
