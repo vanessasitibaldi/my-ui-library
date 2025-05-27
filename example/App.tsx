7d64e59491f476d82f648abc03cbc6416ca6e839
vanessasitibaldi <vanessasitibaldi>
2025-05-27 20:43:14 -0300

    tokens versao nova

--- example/App.tsx
+++ example/App.tsx
@@ -1,38 +1,21 @@
-import { SafeAreaView, ScrollView, Text } from 'react-native';
-import { Button } from '@vanessa.sitibaldi/mylibraryteste';
+import { ThemeProvider } from '@vanessa.sitibaldi/mylibraryteste';
+import { SafeAreaView, ScrollView } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Vanessa</Text>
-        <Button title="Click me" />
-      </ScrollView>
-    </SafeAreaView>
+    <ThemeProvider initialTheme="brand-a">
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Home />
+        </ScrollView>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
 const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-  },
-  view: {
-    flex: 1,
-    height: 200,
   },
 };
